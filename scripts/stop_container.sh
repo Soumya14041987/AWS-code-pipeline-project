2ea2c008dbc4a7d05365bd190568af0628a53def
Soumyadip <96393657+Soumya14041987@users.noreply.github.com>
2024-05-24 02:00:40 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-containerID=`docker ps | awk -F " " '{Print $1}'`
-docker rm -f $containerID
+set -e
+
+#Stop the running container  (if any)
+containerid=`docker ps | awk -F " " '{print $1}'`
+docker rm -f $containerid
